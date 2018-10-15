1bf60a26631db17b4c351571fc65d5198b734539
Virginia Dooley <vcdooley@gmail.com>
2018-10-15 08:53:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,27 @@ class String
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end 
   end
 
-  def count_sentences
-
-  end
+  def count_sentences(arr)
+    arr = self.split
+    if arr.count == 0 
+      puts "zero" 
+    else 
+      arr.count  
+    end
+  end 
 end
