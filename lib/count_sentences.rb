47d18a24d49a8c9d1273953210bc3291e0fb2bfc
Patrick Shim <patrickshim@Patricks-MBP.fios-router.home>
2018-10-17 13:59:20 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,18 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
 end
